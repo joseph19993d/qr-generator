825ed50cf17368b8bd4d59e1cedb75f7f7ad767b
JosephSimbiotico <rodelosuaresj@gmail.com>
2024-03-06 08:49:09 -0500

    cambiar eso

--- src/App.js
+++ src/App.js
@@ -208,8 +208,8 @@ function App() {
                         <path d="M12.5 9.49989C12.1672 8.83131 11.6565 8.8905 11.1407 8.8905C10.2188 8.8905 8.78125 9.99478 8.78125 12.05C8.78125 13.7343 9.52345 15.578 12.0244 18.3361C14.438 20.9979 17.6094 22.3748 20.2422 22.3279C22.875 22.2811 23.4167 20.0154 23.4167 19.2503C23.4167 18.9112 23.2062 18.742 23.0613 18.696C22.1641 18.2654 20.5093 17.4631 20.1328 17.3124C19.7563 17.1617 19.5597 17.3656 19.4375 17.4765C19.0961 17.8018 18.4193 18.7608 18.1875 18.9765C17.9558 19.1922 17.6103 19.083 17.4665 19.0015C16.9374 18.7892 15.5029 18.1511 14.3595 17.0426C12.9453 15.6718 12.8623 15.2001 12.5959 14.7803C12.3828 14.4444 12.5392 14.2384 12.6172 14.1483C12.9219 13.7968 13.3426 13.254 13.5313 12.9843C13.7199 12.7145 13.5702 12.305 13.4803 12.05C13.0938 10.953 12.7663 10.0347 12.5 9.49989Z" fill="white"></path>
                         <defs>
                           <linearGradient id="paint0_linear_87_7264" x1="26.5" y1="7" x2="4" y2="28" gradientUnits="userSpaceOnUse">
-                            <stop stop-color="gray"></stop>
-                            <stop offset="1" stop-color="gray"></stop>
+                            <stop stop-color="purple"></stop>
+                            <stop offset="1" stop-color="purple"></stop>
                           </linearGradient>
                         </defs>
                       </g>
@@ -275,11 +275,13 @@ function App() {
                 <ul className="mt-4 grid gap-4 sm:grid-cols-2 lg:grid-cols-4  p-2  animate__animated ">
                   {/* AQUI COMIENZA LA IMAGEN QR*/}
                   {qrCodes.map((code, index) => (
-                    <div key={index} className='group block overflow-hidden  bg-purple-600 rounded-b-[10px]  w-[265px] h-[365px] m-auto'>
-                      <div id={`qr-code-${index}`} className=' bg-none w-[265px] h-[265px]  p-1 '>
-                        <QRCode value={code}  />
+                    <div key={index} className='group block overflow-hidden  bg-purple-600 rounded-b-[10px] rounded-t-[10px] w-[285px] h-[395px] m-auto p-[10px]'>
+                      <div id={`qr-code-${index}`} className=' bg-none w-[265px] h-[265px]  p-1 flip-2-hor-bottom-1'>
+                        <QRCode value={code} bgColor={'rgba(250, 250, 250, 0)'}  />
                       </div>
-                      <button onClick={() => handleDownloadQRCode(index)}  className='  ml-2 bg-purple-900 px-4 py-3 text-white rounded   '>Descargar</button>
+                      <button onClick={() => handleDownloadQRCode(index)}  className='  ml-2 bg-purple-900 px-4 py-3 text-white rounded mt-2  '>Download</button>
+                      <button className='  ml-2 bg-purple-900 px-4 py-3 text-white rounded mt-2  '>Delete</button>
+
                       <div><a href={code} className=' underline'>{code}</a></div>
 
                     </div>
